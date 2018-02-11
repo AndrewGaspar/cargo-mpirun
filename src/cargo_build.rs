0c90e31068fa5ef9109fb32a601f7156cd08431b
Andrew Gaspar <andrew.gaspar@outlook.com>
2018-02-11 16:45:54 -0700

    cargo build moved to new file

---
+++ src/cargo_build.rs
@@ -0,0 +1,72 @@
+use clap::ArgMatches;
+
+use std::process::{self, Command, Stdio};
+
+static CARGO_OPTIONS: &[&str] = &[
+    "bin",
+    "example",
+    "package",
+    "jobs",
+    "release",
+    "target",
+    "manifest-path",
+    "color",
+    "message-format",
+    "Z",
+];
+
+static CARGO_OPTIONS_MULTIPLE_VALUES: &[&str] = &["features"];
+
+static CARGO_FLAGS: &[&str] = &[
+    "all-features",
+    "no-default-features",
+    "quiet",
+    "frozen",
+    "locked",
+];
+
+static CARGO_FLAGS_MULTIPLE: &[&str] = &["verbose"];
+
+pub fn run<'a>(matches: &ArgMatches<'a>) {
+    let mut cargo_build = Command::new("cargo");
+    cargo_build.arg("build");
+
+    for option in CARGO_OPTIONS {
+        if let Some(value) = matches.value_of(option) {
+            cargo_build.arg("--".to_owned() + option).arg(value);
+        }
+    }
+
+    for option in CARGO_OPTIONS_MULTIPLE_VALUES {
+        if let Some(values) = matches.values_of(option) {
+            cargo_build.arg("--".to_owned() + option).args(values);
+        }
+    }
+
+    for flag in CARGO_FLAGS {
+        if matches.is_present(flag) {
+            cargo_build.arg("--".to_owned() + flag);
+        }
+    }
+
+    for flag in CARGO_FLAGS_MULTIPLE {
+        for _ in 0..matches.occurrences_of(flag) {
+            cargo_build.arg("--".to_owned() + flag);
+        }
+    }
+
+    let status = cargo_build
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .status()
+        .expect("Failed to execute cargo build - is cargo in your path?");
+
+    if !status.success() {
+        let code = match status.code() {
+            Some(x) => x,
+            None => 127,
+        };
+        process::exit(code);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,17 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App, ArgMatches};
+use clap::App;
 
-use std::process::{self, Command, Stdio};
-
-static CARGO_OPTIONS: &[&str] = &[
-    "bin",
-    "example",
-    "package",
-    "jobs",
-    "release",
-    "target",
-    "manifest-path",
-    "color",
-    "message-format",
-    "Z",
-];
-
-static CARGO_OPTIONS_MULTIPLE_VALUES: &[&str] = &["features"];
-
-static CARGO_FLAGS: &[&str] = &[
-    "all-features",
-    "no-default-features",
-    "quiet",
-    "frozen",
-    "locked",
-];
-
-static CARGO_FLAGS_MULTIPLE: &[&str] = &["verbose"];
-
-fn build_target<'a>(matches: &ArgMatches<'a>) {
-    let mut cargo_build = Command::new("cargo");
-    cargo_build.arg("build");
-
-    for option in CARGO_OPTIONS {
-        if let Some(value) = matches.value_of(option) {
-            cargo_build.arg("--".to_owned() + option).arg(value);
-        }
-    }
-
-    for option in CARGO_OPTIONS_MULTIPLE_VALUES {
-        if let Some(values) = matches.values_of(option) {
-            cargo_build.arg("--".to_owned() + option).args(values);
-        }
-    }
-
-    for flag in CARGO_FLAGS {
-        if matches.is_present(flag) {
-            cargo_build.arg("--".to_owned() + flag);
-        }
-    }
-
-    for flag in CARGO_FLAGS_MULTIPLE {
-        for _ in 0..matches.occurrences_of(flag) {
-            cargo_build.arg("--".to_owned() + flag);
-        }
-    }
-
-    let status = cargo_build
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status()
-        .expect("Failed to execute cargo build - is cargo in your path?");
-
-    if !status.success() {
-        let code = match status.code() {
-            Some(x) => x,
-            None => 127,
-        };
-        process::exit(code);
-    }
-}
+mod cargo_build;
 
 fn main() {
     let cli = load_yaml!("cli.yml");
     let matches = App::from_yaml(cli).get_matches();
 
-    if !matches.is_present("mpirun") {
-        println!("Only the 'mpirun' sub-command is implemented.");
-        process::exit(-1);
-    }
+    let matches = matches
+        .subcommand_matches("mpirun")
+        .expect("Only the 'mpirun' sub-command is implemented.");
 
-    let matches = matches.subcommand_matches("mpirun").unwrap();
-
-    build_target(&matches);
+    cargo_build::run(&matches);
 }
