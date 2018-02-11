d7e1bcf27e2f506f19d323d3c5d9488c12ea89b5
Andrew Gaspar <andrew.gaspar@outlook.com>
2018-02-11 13:34:37 -0700

    Add Clap-based CLI that emulates `cargo run`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = load_yaml!("cli.yml");
+    let _matches = App::from_yaml(cli)
+        .arg(
+            Arg::with_name("args") // args specified manually because of clap issue #1173
+                .last(true)
+                .multiple(true)
+                .allow_hyphen_values(true),
+        )
+        .get_matches();
 }
