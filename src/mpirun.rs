b80f4cfdc83ca2805062e0a068183fb2390d0658
Andrew Gaspar <andrew.gaspar@outlook.com>
2018-02-11 19:27:46 -0700

    Use mpiexec rather than mpirun, along with additional enhancements

--- src/mpirun.rs
+++ src/mpirun.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use std::process::{self, Command};
 
 pub fn run(executable: &Path, matches: &ArgMatches) -> ! {
-    let mut mpirun = Command::new("mpirun");
+    let mut mpirun = Command::new("mpiexec");
 
     if let Some(np) = matches.value_of("num_processes") {
         mpirun.arg("--np").arg(np);
@@ -17,7 +17,13 @@ pub fn run(executable: &Path, matches: &ArgMatches) -> ! {
         mpirun.arg("--oversubscribe");
     }
 
-    let code = match mpirun.arg(executable).status() {
+    mpirun.arg(executable);
+
+    if matches.is_present("verbose") {
+        eprintln!("Running: {:?}", mpirun);
+    }
+
+    let code = match mpirun.status() {
         Ok(status) => status.code().unwrap_or(-1),
         Err(_) => {
             eprintln!("Could not find mpirun - is it in your user path?");
