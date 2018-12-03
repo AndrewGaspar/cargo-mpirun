e1076b20c8b8289e7b7c9290601c47f88521ed1b
Andrew Gaspar <andrew.gaspar@outlook.com>
2018-12-02 21:46:08 -0700

    Checks existence of default bin target before building, especially supporting the simple `cargo mpirun` case Fixes #2

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,20 @@ use std::path::*;
 mod cargo_build;
 mod mpirun;
 
-use cargo_metadata::{Metadata, Package};
+use cargo_metadata::{Metadata, Package, Target};
 
-fn find_target(metadata: &Metadata, args: &ArgMatches) -> PathBuf {
+struct ApplicableTarget<'a> {
+    target_type: &'static str,
+    target: &'a Target,
+}
+
+fn get_applicable_target<'a>(metadata: &'a Metadata, args: &'a ArgMatches) -> ApplicableTarget<'a> {
     let packages: Vec<&Package> = {
         if let Some(name) = args.value_of("package") {
             vec![metadata.packages.iter().find(|p| p.name == name).unwrap()]
         } else {
             metadata.packages.iter().collect()
         }
-    };
-
-    let target_directory = Path::new(&metadata.target_directory);
-    let executable_path = if args.is_present("release") {
-        target_directory.join("release")
-    } else {
-        target_directory.join("debug")
     };
 
     if let Some(bin) = args.value_of("bin") {
@@ -44,7 +42,10 @@ fn find_target(metadata: &Metadata, args: &ArgMatches) -> PathBuf {
             std::process::exit(101);
         }
 
-        executable_path.join(bin)
+        ApplicableTarget {
+            target_type: "bin",
+            target: matching_bins[0],
+        }
     } else if let Some(example) = args.value_of("example") {
         // Validates example actually exists
         let examples: Vec<_> = packages
@@ -63,7 +64,10 @@ fn find_target(metadata: &Metadata, args: &ArgMatches) -> PathBuf {
             std::process::exit(101);
         }
 
-        executable_path.join("examples").join(example)
+        ApplicableTarget {
+            target_type: "example",
+            target: examples[0],
+        }
     } else {
         // Get all bins targets in workspace
         let bin_targets: Vec<_> = packages
@@ -89,7 +93,25 @@ fn find_target(metadata: &Metadata, args: &ArgMatches) -> PathBuf {
             std::process::exit(101);
         }
 
-        executable_path.join(&bin_targets[0].name)
+        ApplicableTarget {
+            target_type: "bin",
+            target: bin_targets[0],
+        }
+    }
+}
+
+fn find_target(metadata: &Metadata, args: &ArgMatches, target: &ApplicableTarget) -> PathBuf {
+    let target_directory = Path::new(&metadata.target_directory);
+    let executable_path = if args.is_present("release") {
+        target_directory.join("release")
+    } else {
+        target_directory.join("debug")
+    };
+
+    match target.target_type {
+        "bin" => executable_path.join(&target.target.name),
+        "example" => executable_path.join("examples").join(&target.target.name),
+        _ => unimplemented!(),
     }
 }
 
@@ -101,11 +123,14 @@ fn main() {
         .subcommand_matches("mpirun")
         .expect("Only the 'mpirun' sub-command is implemented.");
 
-    cargo_build::run(&matches);
     let metadata =
         cargo_metadata::metadata(matches.value_of("manifest-path").map(Path::new)).unwrap();
 
-    let target = find_target(&metadata, matches);
+    let target = get_applicable_target(&metadata, &matches);
+
+    cargo_build::run(&matches);
+
+    let target = find_target(&metadata, matches, &target);
 
     mpirun::run(&target, matches);
 }
