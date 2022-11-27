7e0b618c24c48dd1269c16fbd1bd538100cf7a78
Jed Brown <jed@jedbrown.org>
2022-11-27 08:37:30 -0700

    Split out lib.rs and main.rs, upgrade clap and use thiserror
    Adopt a technique similar to cargo-flamegraph for parsing cargo build
    output; fix #6.

--- src/mpirun.rs
+++ src/mpirun.rs
@@ -1,39 +1,25 @@
-use clap::ArgMatches;
 use std::path::Path;
-use std::process::{self, Command};
+use std::process::Command;
 
-pub fn run(executable: &Path, matches: &ArgMatches) -> ! {
+/// Build command to run MPI job
+pub fn run_command(executable: &Path, args: &crate::Args) -> Command {
     let mut mpirun = Command::new("mpiexec");
 
-    if let Some(np) = matches.value_of("num_processes") {
-        mpirun.arg("-n").arg(np);
+    if let Some(n) = args.num_processes {
+        mpirun.arg("-n").arg(n.to_string());
     }
-
-    if let Some(n) = matches.value_of("num_processes_per_node") {
-        mpirun.arg("--npernode").arg(n);
+    if let Some(n) = args.num_processes_per_node {
+        mpirun.arg("--npernode").arg(n.to_string());
     }
-
-    if matches.is_present("oversubscribe") {
+    if args.oversubscribe {
         mpirun.arg("--oversubscribe");
     }
 
     mpirun.arg(executable);
+    mpirun.args(&args.args);
 
-    if let Some(args) = matches.values_of("args") {
-        mpirun.args(args);
-    }
-
-    if matches.is_present("verbose") {
+    if args.verbose > 0 {
         eprintln!("Running: {:?}", mpirun);
     }
-
-    let code = match mpirun.status() {
-        Ok(status) => status.code().unwrap_or(-1),
-        Err(_) => {
-            eprintln!("Could not find mpiexec - is it in your user path?");
-            -1
-        }
-    };
-
-    process::exit(code)
+    mpirun
 }
